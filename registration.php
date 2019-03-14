0d2ca2a603fdcf0c94c0363808dcedd8a89d2a3a
Sarvesh <33909592+Sarvesh-A@users.noreply.github.com>
2019-03-14 17:02:34 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* Copyright © 2015 Magento. All rights reserved.
+* Copyright © 2019 All rights reserved.
 * See COPYING.txt for license details.
 */
 \Magento\Framework\Component\ComponentRegistrar::register(
